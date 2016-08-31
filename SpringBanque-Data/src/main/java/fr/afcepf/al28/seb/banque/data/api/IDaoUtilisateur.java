0b5cab3ee6d5c37cd4fef735fcb1fb997789bc18
malaghope <chocoultrachoc@yahoo.fr>
2016-08-31 16:14:40 +0200

    beaucoup de modifs

--- SpringBanque-Data/src/main/java/fr/afcepf/al28/seb/banque/data/api/IDaoUtilisateur.java
+++ SpringBanque-Data/src/main/java/fr/afcepf/al28/seb/banque/data/api/IDaoUtilisateur.java
@@ -3,8 +3,7 @@ package fr.afcepf.al28.seb.banque.data.api;
 import fr.afcepf.al28.seb.banque.entity.Utilisateur;
 
 public interface IDaoUtilisateur {
-
-	public Utilisateur addUtil();
-    public Boolean removeUtil();
-    public Utilisateur updateUtil(Utilisateur util);
+	public Utilisateur addUtilisateur(Utilisateur utilisateur);
+    public Boolean removeUtilisateur(Utilisateur utilisateur);
+    public Utilisateur updateUtilisateur(Utilisateur utilisateur);
 }
---
+++ SpringBanque-Data/src/main/java/fr/afcepf/al28/seb/banque/data/impl/DaoUtilisateurImpl.java
@@ -0,0 +1,35 @@
+package fr.afcepf.al28.seb.banque.data.impl;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import fr.afcepf.al28.seb.banque.data.api.IDaoUtilisateur;
+import fr.afcepf.al28.seb.banque.entity.Utilisateur;
+@Service
+@Transactional
+public class DaoUtilisateurImpl implements IDaoUtilisateur{
+    private SessionFactory sf;
+    @Autowired
+    public void setSf(SessionFactory sf){
+        this.sf = sf;
+    }
+    @Override
+    public Utilisateur addUtilisateur(Utilisateur paramUtilisateur) {
+        sf.getCurrentSession().save(paramUtilisateur);
+        return paramUtilisateur;
+    }
+
+    @Override
+    public Boolean removeUtilisateur(Utilisateur paramUtilisateur) {
+        sf.getCurrentSession().delete(paramUtilisateur);
+        return true;
+    }
+    
+    @Override
+    public Utilisateur updateUtilisateur(Utilisateur paramUtilisateur) {
+        sf.getCurrentSession().update(paramUtilisateur);
+        return paramUtilisateur;
+    }
+}
