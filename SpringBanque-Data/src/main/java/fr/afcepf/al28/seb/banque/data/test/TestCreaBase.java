536142cc49eb270a17144c03b21a9abf8e4242d7
b <b@rocky5Mint>
2016-08-31 21:15:36 +0200

    Commit

--- SpringBanque-Data/src/main/java/fr/afcepf/al28/seb/banque/data/test/TestCreaBase.java
+++ SpringBanque-Data/src/main/java/fr/afcepf/al28/seb/banque/data/test/TestCreaBase.java
@@ -4,9 +4,16 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import fr.afcepf.al28.seb.banque.data.api.IDaoUtilisateur;
+import fr.afcepf.al28.seb.banque.entity.Utilisateur;
+
 public class TestCreaBase {
     private static Logger log = Logger.getLogger(TestCreaBase.class);
     public static void main(String[] args) {
         BeanFactory bf = new ClassPathXmlApplicationContext("classpath:spring.xml");
+        IDaoUtilisateur dao = bf.getBean(IDaoUtilisateur.class);
+        Utilisateur util = new Utilisateur();
+        util.setNom("caca");
+        dao.addUtilisateur(util);
     }
 }
---
+++ SpringBanque-Front-JSF/src/main/java/fr/afcepf/al28/seb/banque/front/ConnexionMB.java
@@ -0,0 +1,5 @@
+package fr.afcepf.al28.seb.banque.front;
+
+public class ConnexionMB {
+
+}
